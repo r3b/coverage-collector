7591f4402440010097a238f6cacb2b9e479ce2ef
ryan bridges <rbridges@apigee.com>
2014-04-13 20:35:42 -0400

    Removing some debug logs. Fixing server shutdown.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var http = require("http");
 var connect = require('connect');
 var bodyParser = require('body-parser');
+var server;
 function start(options){
 	options=options||{};
 	port=options.port||process.env['COLLECTOR_PORT']||3001;
 	var app = connect();
 	global._data=global._data||[];
-	app.use(bodyParser());
+	app.use(bodyParser({limit:'50mb'}));
 	app.use(require('./list')());
 	app.use(require('./receive')());
 	app.use(function(req, res, next){
@@ -14,7 +15,10 @@ function start(options){
 			console.log("Shutdown requested");
 			res.statusCode=204;
 			res.end();
-			process.exit();
+			if(server){
+				server.close();
+				server=null;
+			}
 		}else{
 			next();
 		}
@@ -22,7 +26,7 @@ function start(options){
 	app.use(function(req, res){
 	    res.end();
   	});
-	http.createServer(app).listen(port);
+	server=http.createServer(app).listen(port);
 	console.log("Collector started on port %d", port);
 }
 module.exports=start;
--- receive.js
+++ receive.js
@@ -2,12 +2,9 @@ global._data=global._data||[];
 module.exports = function serveCoverageSetup() {
   return function serveCoverageHandle(req, res, next) {
       if(req.url.substr(1)==='data' && req.method.toLowerCase()==='post'){
-        console.log("received post data\n");
-        console.log(req.body);
         global._data.push(req.body);
           res.statusCode=204;
           res.end();
-
       }else{
         next();
       }
